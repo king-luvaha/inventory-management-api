3ccd9f3c92f0b35b2224548643d9bc45ef2b5cb0
Luvaha <luvaha456@gmail.com>
2025-08-26 14:48:27 +0300

    Created new file: Added JWT Authentication endpoints, Added RESTful routes

--- inventory_api/urls.py
+++ inventory_api/urls.py
@@ -10,7 +10,7 @@ router.register(r'inventory', InventoryItemViewSet, basename='inventory')
 router.register(r'changes', InventoryChangeViewSet, basename='changes')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
