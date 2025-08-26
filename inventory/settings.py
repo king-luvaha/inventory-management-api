306122a957b00e037f86c3ebef0562ae53391d7b
Luvaha <luvaha456@gmail.com>
2025-08-26 14:55:32 +0300

    Added 'inventory_api' and 'django_filters' to INSTALLED_APPS section

--- inventory/settings.py
+++ inventory/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'inventory',
+    'inventory_api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
