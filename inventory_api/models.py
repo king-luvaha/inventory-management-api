068ededa48cf2c0c0bf0d01f9691724fe401b011
Luvaha <luvaha456@gmail.com>
2025-08-26 12:04:06 +0300

    Defined Category Model

--- inventory_api/models.py
+++ inventory_api/models.py
@@ -7,3 +7,10 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
