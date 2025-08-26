a113ef6dba049da58dd5b0f1e98c5eed2f8cc8e6
Luvaha <luvaha456@gmail.com>
2025-08-26 18:34:58 +0300

    Updated token refresh lifetime from 1 day to 30 days

--- inventory/settings.py
+++ inventory/settings.py
@@ -141,5 +141,5 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
