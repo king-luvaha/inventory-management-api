96e94a52b97f75642e8f4d247045143d0697b730
Luvaha <luvaha456@gmail.com>
2025-08-26 13:44:13 +0300

    Added import statements, Defined UserViewsSet

--- inventory_api/views.py
+++ inventory_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import InventoryItem, Category, InventoryChange, User
+from .serializers import (
+    InventoryItemSerializer,
+    CategorySerializer,
+    InventoryChangeSerializer,
+    UserSerializer
+)
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
