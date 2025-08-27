da95a103145e46144bcebca0ed44b343260153a0
Luvaha <luvaha456@gmail.com>
2025-08-27 10:48:53 +0300

    Added 'verbose_name_plural' to neatly display 'Categories' instead of 'Categorys'

--- inventory_api/models.py
+++ inventory_api/models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
     def __str__(self):
         return self.name
     
